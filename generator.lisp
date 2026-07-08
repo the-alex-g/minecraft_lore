c8b12d3aadc00294668c75f84eaba58b418df6d5
Alex Gestwicki <alex.gestwicki@gmail.com>
2026-07-08 08:48:37 -0400

    Add echoing sculk lore page

---
+++ generator.lisp
@@ -0,0 +1,22 @@
+(defun get-file-as-string (pathname)
+  (with-output-to-string (out)
+    (with-open-file (in pathname)
+      (loop with buffer = (make-array 8192 :element-type 'character)
+	    for n-characters = (read-sequence buffer in)
+	    while (< 0 n-characters)
+	    do (write-sequence buffer out :start 0 :end n-characters)))
+    out))
+
+(defun get-link-element (path)
+  (format nil "<a href=\"lore_pages/~a\">~:(~a~)</a>"
+	  (file-namestring path)
+	  (substitute #\space #\_ (pathname-name path))))
+
+(defun build-index ()
+  (with-open-file (stream "index.html" :direction :output
+				       :if-exists :supersede
+				       :if-does-not-exist :create)
+    (format stream (get-file-as-string "index_template.html")
+	    (mapcar #'get-link-element (directory "lore_pages/*.html")))))
+
+(build-index)
